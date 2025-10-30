2abd420b7748d1b222cd4417a6c6ce9ee864b06d
Dias Maksatov <diasmaksatov@MacBook-Air-Dias.local>
2025-10-30 16:23:39 +0500

    update

--- app/core/security.py
+++ app/core/security.py
@@ -13,7 +13,7 @@ def verify_password(plain_password: str, hashed_password: str)->bool:
     return pwd_context.verify(plain_password,hashed_password)
 
 def create_access_token(data:dict)->str:
-    to_encode = data.copy
+    to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode,settings.SECRET_KEY,algorithm=settings.ALGORITHM)
