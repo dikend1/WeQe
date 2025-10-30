2a94c0ea2765267009591874d51e76916698bfa8
Dias Maksatov <diasmaksatov@MacBook-Air-Dias.local>
2025-10-30 13:51:33 +0500

    added schemas

---
+++ app/schemas/__init__.py
@@ -0,0 +1,3 @@
+from .comment_schema import CommentBase,CommentCreate,CommentResponse
+from .post_schema import PostBase,PostCreate,PostResponse
+from .user_schema import UserBase,UserCreate,UserResponse
